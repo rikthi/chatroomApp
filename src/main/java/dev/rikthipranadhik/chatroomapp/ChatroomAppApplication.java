4b566a3029cd21425edc22575b89888d4296b15c
Rikthi Pranadhik Mondal <147805511+rikthi@users.noreply.github.com>
2024-09-19 22:12:29 -0600

    Initial Commit

---
+++ src/main/java/dev/rikthipranadhik/chatroomapp/ChatroomAppApplication.java
@@ -0,0 +1,13 @@
+package dev.rikthipranadhik.chatroomapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatroomAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatroomAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rikthipranadhik/chatroomapp/ChatroomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rikthipranadhik.chatroomapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatroomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
